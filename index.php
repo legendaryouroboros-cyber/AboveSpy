7e6fa6372ae64a341d2578696b9c21ed08059a20
Бекжан <legendaryouroboros@gmail.com>
2026-04-09 03:50:37 +0600

    ss

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ $token = '8704126137:AAFKWtfeYJq2A9LVAlJOoM-nMAnmzVhFGhY';
 $botUrl = "https://api.telegram.org/bot{$token}";
 
 $input = file_get_contents('php://input');
-error_log("INPUT: " . $input);
+
+file_put_contents('/tmp/log.txt', date('H:i:s') . " INPUT: " . $input . "\n", FILE_APPEND);
 
 $update = json_decode($input, true);
 
 if (!isset($update['message'])) {
-    echo "OK";
+    echo file_get_contents('/tmp/log.txt');
     exit;
 }
 
@@ -31,7 +32,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 $result = curl_exec($ch);
-error_log("RESULT: $result");
+file_put_contents('/tmp/log.txt', "RESULT: $result\n", FILE_APPEND);
 curl_close($ch);
 
 http_response_code(200);
